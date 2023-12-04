a9cbb998830929d7351b27e5cdfad48974309889
ghkden55 <ghkden55@gmail.com>
2023-12-04 18:16:44 +0900

    231204
    23년 12월 04일 수업 내용
    쇼핑몰 - 상품

---
+++ product/src/main/java/com/example/product/product/Product.java
@@ -0,0 +1,37 @@
+package com.example.product.product;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Product {
+
+    // PK
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 상품명, 입력 값 필수
+    @Column(length = 100, nullable = false)
+    private String productName;
+
+    // 상품 설명, 입력 값 필수
+    @Column(length = 500, nullable = false)
+    private String description;
+
+    // 이미지 정보
+    @Column(length = 100)
+    private String image;
+
+    // 가격
+    private int price;
+
+    // 수량
+    //private int quantity;
+
+}
+
---
+++ product/src/main/java/com/example/product/product/ProductResponse.java
@@ -0,0 +1,88 @@
+package com.example.product.product;
+
+import com.example.product.option.Option;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//@NoArgsConstructor
+public class ProductResponse {
+
+
+    @Getter
+    @Setter
+    public static class FindAllDTO {
+        private Long id;
+
+        private String productName;
+
+        private String description;
+
+        private String image;
+
+        private int price;
+
+        //private int quantity; // 수량은 옵션에 들어갈 것 이므로 나중에 삭제 가능성
+
+        public FindAllDTO(Product product) {
+            this.id = product.getId();
+            this.productName = product.getProductName();
+            this.description = product.getDescription();
+            this.image = product.getImage();
+            this.price = product.getPrice();
+        }
+
+    }
+
+
+    @Getter
+    @Setter
+    public static class FindByIdDTO{
+        private Long id;
+
+        private String productName;
+
+        private String description;
+
+        private String image;
+
+        private int price;
+
+        private List<OptionDTO> optionList;
+
+        //private int quantity; // 수량은 옵션에 들어갈 것 이므로 나중에 삭제 가능성
+
+        public FindByIdDTO(Product product, List<Option> optionList) {
+            this.id = product.getId();
+            this.productName = product.getProductName();
+            this.description = product.getDescription();
+            this.image = product.getImage();
+            this.price = product.getPrice();
+            this.optionList = optionList.stream().map(OptionDTO::new)
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+
+    @Getter
+    @Setter
+    public static class OptionDTO {
+        private Long id;
+        private String optionName;
+        private Long Price;
+        private Long quantity;
+
+        public OptionDTO(Option option) {
+            this.id = option.getId();
+            this.optionName = option.getOptionName();
+            this.Price = option.getPrice();
+            quantity = option.getQuantity();
+        }
+
+    }
+
+}
