b1c55f5d57c2900ebd2f927d2ec0d7aaf3763096
ghkden55 <ghkden55@gmail.com>
2023-12-08 18:09:30 +0900

    create html controller
    타임리프 사용을 위한 컨트롤러 일부 생성

---
+++ product/src/main/java/com/example/product/pageController/UserPageController.java
@@ -0,0 +1,21 @@
+package com.example.product.pageController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserPageController {
+
+    @GetMapping("/")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/joinForm")
+    public String join() {
+        return "joinForm";
+    }
+
+}
